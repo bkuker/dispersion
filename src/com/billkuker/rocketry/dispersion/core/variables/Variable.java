3a004075309b04fcdfe2733636a960d8b096ace3
bkuker <bkuker@billkuker.com>
2014-02-20 16:13:19 -0500

    Don't like where that was going

--- src/com/billkuker/rocketry/dispersion/core/variables/Variable.java
+++ src/com/billkuker/rocketry/dispersion/core/variables/Variable.java
@@ -1,25 +1,10 @@
 package com.billkuker.rocketry.dispersion.core.variables;
 
-import java.util.Map;
 import java.util.Random;
 
 public abstract class Variable extends Number {
 	private static final long serialVersionUID = 1L;
 	private static final Random r = new Random(0);
-
-	private String name;
-
-	public static void setName(final Number n, final String name) {
-		if (n instanceof Variable) {
-			((Variable) n).setName(name);
-		}
-	}
-
-	private static ThreadLocal<Map<String, Double>> values = new ThreadLocal<Map<String, Double>>();
-
-	public void setVariableValueMap(Map<String, Double> map) {
-
-	}
 
 	protected double randomDouble() {
 		return r.nextDouble();
@@ -27,12 +12,6 @@ public abstract class Variable extends Number {
 
 	protected double randomGaussian() {
 		return r.nextDouble();
-	}
-
-	void setName(final String name) {
-		if (this.name != null)
-			throw new IllegalStateException("This " + getClass().getSimpleName() + " already assigned name " + name);
-		this.name = name;
 	}
 
 	@Override
